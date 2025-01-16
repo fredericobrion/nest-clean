f52ac7d560f7d338b347d2b61cae6c61d28f4caf
frederico <fredericoasbrion@gmail.com>
2025-01-16 18:37:50 -0300

    feat: upload file

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,25 +1,39 @@
 // ? Esse arquivo é executado antes de cada teste E2E
 
-import 'dotenv/config'
+import { config } from 'dotenv'
+
 import { PrismaClient } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
+
+// O Nest ainda não faz o override de envs, então precisa forçar para testes
+config({ path: '.env', override: true })
+config({ path: '.env.test', override: true })
+
 const prisma = new PrismaClient()
+
 function generateUniqueDatabaseURL(schemaId: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide a DATABASE_URL environment variable')
+    throw new Error('Please provider a DATABASE_URL environment variable')
   }
+
   const url = new URL(process.env.DATABASE_URL)
+
   url.searchParams.set('schema', schemaId)
+
   return url.toString()
 }
+
 const schemaId = randomUUID()
-// Sobscreve a env antes dos testes
+
 beforeAll(async () => {
   const databaseURL = generateUniqueDatabaseURL(schemaId)
+
   process.env.DATABASE_URL = databaseURL
-  execSync('npm prisma migrate deploy')
+
+  execSync('pnpm prisma migrate deploy')
 })
+
 afterAll(async () => {
   await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
   await prisma.$disconnect()
